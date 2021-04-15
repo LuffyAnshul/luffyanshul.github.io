9b20976523840ab12c3a38747b63b86564d3eeab
LuffyAnshul <anshulborawake@gmail.com>
2021-04-15 20:48:19 +0530

    Info Complete

--- forms/contact.php
+++ forms/contact.php
@@ -24,14 +24,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
+    'host' => 'https://luffyanshul.github.io',
+    'username' => 'contactgraffictraffic@gmail.com',
+    'password' => 'D4nshul2000@graffictraffic',
+    'port' => '993'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
